6df35d690c762f814ca6b95c52ca1c24cd4a4f14
Orzu <orzumaminov9@gmail.com>
2024-02-24 05:33:59 +0800

    route updated

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <div>
+                <h1>Admin Panel</h1>
+                <Link to="text_admin">Admin Panel</Link>
+            </div>
+        ),
+    },
+    {
+        path: "text_admin",
+        element: <App/>,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <RouterProvider router={router} />
 );
