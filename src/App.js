766925ed2f8fc8f9e2d7a9ec6031e6dc85d6c6d3
Orzu <orzumaminov9@gmail.com>
2024-02-24 03:53:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [formData, setFormData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://94.131.121.176:8000/get_bot_text'); // ЭТО ЗАПРОС К СЕРВЕРУ
+                console.log(response.data.data)
+                setFormData(response.data.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    async function handleSubmit(pk, name, value) {
+        try {
+            const response = await axios.post(
+                'http://94.131.121.176:8000/update_bot_text',
+                { pk, name, value },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            console.log('Data submitted successfully!');
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('Error submitting data:', error);
+        }
+    }
+
+    function handleTextChange(pk, e) {
+        const newData = formData.map(item => {
+            if (item.pk === pk) {
+                return { ...item, value: e.target.value };
+            }
+            return item;
+        });
+        setFormData(newData);
+    }
+
+    return (
+        <div className="App">
+            {formData.map((formInfo) => (
+                <Card key={formInfo.pk} style={{ width: '18rem', margin: '2rem' }}>
+                    <Card.Body>
+                        <Card.Title>{formInfo.pk}</Card.Title>
+                        <Card.Text>
+                            <Form>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>{formInfo.name}</Form.Label>
+                                    <Form.Control
+                                        as="textarea"
+                                        rows={3}
+                                        value={formInfo.value}
+                                        onChange={(e) => handleTextChange(formInfo.pk, e)}
+                                    />
+                                </Form.Group>
+                                <Button
+                                    type="button"
+                                    onClick={() => handleSubmit(formInfo.pk, formInfo.name, formInfo.value)}
+                                >
+                                    Сохранить
+                                </Button>
+                            </Form>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            ))}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/axiosConfig.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://94.131.121.176:8000';
+axios.defaults.withCredentials = true; // Разрешить отправку куки
